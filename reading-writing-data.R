50e403e6ab8bfcebd64b20764293459d645e21ef
zhou-dong <82224165@qq.com>
2015-02-11 01:12:50 -0600

    update reading writing

--- reading-writing-data.R
+++ reading-writing-data.R
@@ -1,12 +1,11 @@
-# reading
+# (1): reading
 # read.table and read.cvs   for reading tabular data
 # readLines                 for reading line of text file
 # source                    for reading in R code file (inverse of dump)
 # dget                      for reading in R code file (inverse of dput)
 # load                      for reading in saved workspaces
 # unserialize               for reading in single R object in binary form
-
-# writing
+# (2): writing
 # write.table
 # writeLines
 # dump
