300c12a3964ec7cce39bd6c7051abed7d96ee828
zhou-dong <82224165@qq.com>
2015-02-23 19:29:46 -0600

    sedon

---
+++ cs-6649-second.R
@@ -0,0 +1,7 @@
+# http://www.csc.ncsu.edu/faculty/samatova/practical-graph-mining-with-R/PracticalGraphMiningWithR.html
+
+install.packages("devtools")
+
+library("devtools")
+
+install_local("/Users/dongdong/Downloads/ProximityMeasure.tgz")
