212448fb8ccd6e466f65819f699d9a20a789ef4b
zhou-dong <82224165@qq.com>
2015-02-23 19:59:02 -0600

    update cs 6649

--- cs-6649-first.R
+++ cs-6649-first.R
@@ -19,3 +19,38 @@ gplot(graph,gmode="digraph", displaylabels=TRUE)
 
 # Graph the cars vector with all defaults
 #plot(cars)
+# Class code
+library(sna)
+N=10
+plink=0.15
+graph=rgraph(N,1,plink,"digraph",TRUE)
+gplot(graph,gmode="digraph", displaylabels=TRUE)
+#Degree of an undirected graph
+degree(graph, gmode="digraph")
+
+#Denisty of a graph
+gden(graph, mode="digraph")
+
+#Connectedness
+is.connected(graph)
+connectedness(graph)
+is.isolate(graph,1)
+is.isolate(graph,2)
+
+#Betweenness
+betweenness(graph)
+geo=geodist(graph)
+geo$gdist
+#Egocentricity
+ego.extract(graph,6, neighborhood = c("combined", "in", "out"))
+ego.extract(graph,3, neighborhood = "in")
+#Closeness centrality
+closeness(graph) # a disconnected node can cause this measure to be 0, because that node can't be reached in INF hops
+newGraph= ego.extract(graph,9)
+gplot(newGraph, displaylabels=TRUE)
+closeness(newGraph)
+#Page rank algorithm
+library(igraph)
+g<-random.graph.game(20,5/20, directed=TRUE)
+page.rank(g)$vector
+plot(g)
--- cs-6649-second.R
+++ cs-6649-second.R
@@ -1,7 +1,28 @@
 # http://www.csc.ncsu.edu/faculty/samatova/practical-graph-mining-with-R/PracticalGraphMiningWithR.html
 
+# first part, not work on new version of R stuido
 install.packages("devtools")
 
 library("devtools")
 
 install_local("/Users/dongdong/Downloads/ProximityMeasure.tgz")
+
+install_local("/Users/dongdong/Downloads/LinkAnalysis.tgz")
+
+library(igraph)
+library(ProximityMeasure)
+library(LinkAnalysis)
+A<-matrix(c(0,1,1,0,0,0,
+            1,0,1,1,1,1,
+            0,1,0,0,0,0,
+            0,0,1,0,1,0,
+            0,1,1,0,0,0,
+            0,0,1,0,0,0),
+          nrow=6,ncol=6, byrow=TRUE);
+G<-graph.adjacency(A,mode=c("directed"), weighted= NULL)
+kmax<-6;
+op=HITS(G,kmax);
+
+help(HITS)
+
+# second part use the igraph
