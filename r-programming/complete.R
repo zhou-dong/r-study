2f439386254ae7e7cb6dd6503efbf0959796401e
zhou-dong <82224165@qq.com>
2016-01-15 00:50:30 -0600

    update

---
+++ r-programming/complete.R
@@ -0,0 +1,18 @@
+complete <- function(directory, id=1:332){
+ 
+    dir <- paste("/Users/dongdong/Downloads/", directory, sep = "")
+    dir <- paste(dir, "/", sep = "")
+    
+    nobs <- vector(mode="numeric", length(id))
+    
+    for(x in 1:length(id)){
+        formatId <- sprintf("%03.f", as.numeric(id[x]))
+        filename <- paste(formatId, ".csv", sep = "")
+        filepath <- paste(dir, filename, sep = "") 
+        rough_data <- read.csv(filepath)
+        complete_num <- sum(complete.cases(rough_data))
+        nobs[x] <- complete_num
+    }
+    
+    data.frame(id, nobs)
+}
